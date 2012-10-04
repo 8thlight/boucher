a2c78677320226157026dc0af1ec88812872b9e8
Micah <micahmartin@gmail.com>
2012-10-04 13:25:22 -0700

    assumes server ids start with 'i-'

--- lib/boucher/util.rb
+++ lib/boucher/util.rb
@@ -7,12 +7,15 @@ module Boucher
   end
 
   def resolve_servers(id_or_meal)
-    [Boucher::Servers.with_id(id_or_meal)]
-  rescue
-    puts "Searching for running #{id_or_meal} servers in #{Boucher.env_name} environment..."
-    servers = Boucher::Servers.search(:meal => id_or_meal, :env => Boucher.env_name, :state => "!terminated")
-    Boucher::print_servers[servers]
-    servers
+    if id_or_meal[0..1] == "i-"
+      puts "Retrieving server with id #{id_or_meal}..."
+      [Boucher::Servers.with_id(id_or_meal)]
+    else
+      puts "Searching for running #{id_or_meal} servers in #{Boucher.env_name} environment..."
+      servers = Boucher::Servers.search(:meal => id_or_meal, :env => Boucher.env_name, :state => "!terminated")
+      Boucher::print_servers[servers]
+      servers
+    end
   end
 
 end
