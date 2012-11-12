b9018668ab94ff7aef84c0b1b20650e113a21890
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-11-12 08:24:21 -0600

    Print details for security groups.  Still need to format case with multiple incoming groups

--- lib/boucher/security_groups.rb
+++ lib/boucher/security_groups.rb
@@ -4,18 +4,46 @@ require 'boucher/servers'
 module Boucher
   module SecurityGroups
     SECURITY_GROUP_TABLE_FORMAT = "%-12s  %-12s  %-50s\n"
+    RULE_TABLE_FORMAT = "%-18s  %-19s  %-10s %-10s %-10s\n"
 
     module Printing
       def self.print_table(security_groups, servers_for_groups)
-        printf SECURITY_GROUP_TABLE_FORMAT, "ID", "Name", "Servers"
-        puts
+        puts "----------------------------------------------------------------------"
         security_groups.each do |security_group|
+          puts "GROUP:"
+          printf SECURITY_GROUP_TABLE_FORMAT, "ID", "Name", "Servers"
           printf SECURITY_GROUP_TABLE_FORMAT,
             security_group.group_id,
             security_group.name,
             server_names(servers_for_groups[security_group.name])
+          puts "\n"
+          puts "RULES FOR GROUP:"
+          printf RULE_TABLE_FORMAT, "Incoming Groups", "Incoming IP", "Protocol", "Min Port", "Max Port"
+          security_group.ip_permissions.each do |rule|
+            printf RULE_TABLE_FORMAT, rule_groups(rule), rule_ip(rule), rule["ipProtocol"], rule["fromPort"], rule["toPort"]
+          end
+          puts "----------------------------------------------------------------------"
+          puts "\n"
         end
       end
+      def self.rule_groups(ip_permissions)
+        names = ip_permissions["groups"].map do |group|
+          group["groupName"]
+        end
+        if names.empty?
+          "______"
+        else
+          names.join("\n")
+        end
+      end
+
+      def self.rule_ip(ip_permission)
+        ip = (ip_permission["ipRanges"].first || {})["cidrIp"]
+        ip || "______"
+
+
+      end
+
 
       def self.server_names(servers)
         servers.map { |server|
